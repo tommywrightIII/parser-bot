5e87d64f583046d7787624cf4462853c3030c3f1
tommywrightIII <ramzantemerbiev@gmail.com>
2026-05-05 01:40:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import httpx
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
@@ -20,6 +21,11 @@ dp.include_router(track_router)
 
 async def main():
     logging.info("🤖 Бот запущен")
+    try:
+        r = httpx.get("https://api.ipify.org")
+        logging.info(f"Railway IP: {r.text}")
+    except Exception as e:
+        logging.info(f"IP error: {e}")
     asyncio.create_task(tracking_loop(bot))
     await dp.start_polling(bot, skip_updates=True)
 
